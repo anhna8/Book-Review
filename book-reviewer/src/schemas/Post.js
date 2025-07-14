7079f55d3499c1cdd57ab08965912b3bc4dd1463
anhna <ana_mjl@hotmail.com>
2025-07-13 21:21:44 -0600

    added and install neostandard

---
+++ book-reviewer/src/schemas/Post.js
@@ -0,0 +1,37 @@
+import * as yup from 'yup'
+
+export const postSchema = yup.object({
+  title: yup
+    .string()
+    .required('El título es requerido')
+    .max(100, 'El título no puede exceder 100 caracteres'),
+  content: yup
+    .string()
+    .required('El contenido es requerido')
+    .min(20, 'El contenido debe tener al menos 20 caracteres'),
+  category: yup
+    .string()
+    .required('La categoría es requerida')
+    .oneOf([
+      'Ciencia Ficcion',
+      'Fantasia',
+      'Terror',
+      'Romance',
+      'Thriller',
+      'Novela Histórica',
+      'Aventuras',
+      'Poesía',
+      'Misterio',
+      'Juvenil',
+      'No Ficción',
+      'Biografía',
+    ], 'La categoría debe ser una opción válida'),
+  author: yup
+    .string()
+    .required('El autor es requerido')
+    .min(2, 'El nombre del autor debe tener al menos 2 caracteres'),
+  publishDate: yup
+    .date()
+    .required('La fecha de publicación es requerida')
+    .min(new Date(), 'La fecha debe ser posterior a hoy')
+}).required()
